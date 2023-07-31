e69f671a6d8720d6fd713ba20ede2a013f48649a
jpentinio <joshuapentinio98@gmail.com>
2023-07-31 11:22:17 +0800

    update readme

--- src/store.js
+++ src/store.js
@@ -1,6 +1,29 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
+const initialTodos = [
+  {
+    id: "7bdb7249-a257-4c17-86ea-cd5cd027bb4c",
+    isCompleted: true,
+    text: "Complete online JavaScript course",
+  },
+  {
+    id: "e4d1f3d2-e9ae-4250-b879-91d8be2b0b4f",
+    isCompleted: false,
+    text: "Jog around the park 3x",
+  },
+  {
+    id: "5983039c-f996-4028-8934-4bc17b32f705",
+    isCompleted: false,
+    text: "Complete Todo App on Frontend Mentor",
+  },
+  {
+    id: "211e4dc5-ffb1-4b3d-8e75-8b100f266706",
+    isCompleted: false,
+    text: "10 minutes meditation",
+  },
+];
+
 const setIsCompleted = (todos, id) => {
   for (let i in todos) {
     if (todos[i].id === id) {
@@ -12,7 +35,7 @@ const setIsCompleted = (todos, id) => {
 };
 
 const store = (set) => ({
-  todos: [], //LIST of todos
+  todos: [...initialTodos], //LIST of todos
   setTodos: (todos) => set((store) => ({ todos })),
   filter: "all",
   setFilter: (filter) => set(() => ({ filter })),
